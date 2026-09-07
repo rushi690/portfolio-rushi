15e94b82443179603f47dccf1f37beb945ad7329
Rushikesh Padamwar <rushikeshpadamwar2@gmail.com>
2026-09-07 13:09:39 +0530

    Fix formatting in About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
          a motivated Java Developer and recent graduate skilled in Core Java, OOP, 
          Collections, Multithreading, SQL, JDBC, REST APIs, Spring Boot, Hibernate, MySQL, JavaScript, and React.js. 
          I enjoy building practical applications and solving programming problems with clean, efficient code. 
-         I’m looking for a **Java Developer / Software Developer opportunity to contribute, learn, and grow.
+         I’m looking for a Java Developer / Software Developer opportunity to contribute, learn, and grow.
 
         </p>
       </div>
